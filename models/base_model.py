0925de6f59278d883e56df4ffd8d1029bdc52e34
AdiyoFouad <odjfouad38@gmail.com>
2023-02-07 15:56:38 +0100

    Update BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,29 @@ import uuid
 class BaseModel:
     """Represent a BaseModel."""
 
-    def __init__(self):
-        """Initialize a Base_Model instance."""
+    def __init__(self, *args, **kwargs):
+        """Initialize a Base_Model instance.
+        Args:
+            self (BaseModel): current instance
+            args (any): not used here
+            kwargs (dict) : dictionnary of key/value pairs attributes
+        """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        if len(kwargs):
+            date_format = "%Y-%m-%dT%H:%M:%S.%f" 
+            for key,value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    self.__dict__[key] = datetime.strptime(value, date_format)
+                else:
+                    self.__dict__[key] = value
+                    
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def __str__(self):
-        """Define th print() the print representation of a BasModel"""
+        """Define the print() representation of a BasModel"""
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
