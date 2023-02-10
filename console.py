73afe1a0bfed7343a463b86c8f4b468db2c92f54
AdiyoFouad <odjfouad38@gmail.com>
2023-02-10 11:03:19 +0100

    Update conssole

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module for the entry point of the command interpreter."""
 
-import cmd, models
+import cmd, models, re
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
@@ -30,7 +30,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         """
-        Usage: create <class>
+        Usage: create <class name>
         Create a new instance of BaseModel
         """
         arg = line.split()
@@ -47,7 +47,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, line):
         """
-        Usage: show <class>  <id>
+        Usage: show <class name>  <id>
         Print the string representation of an instance based on the class name and id
         """
 
@@ -69,7 +69,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, line):
         """
-        Usage: destroy <class>  <id>
+        Usage: destroy <class name>  <id>
         Delete an instance based on the class name and id
         """
         args = line.split()
@@ -90,7 +90,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, line):
         """
-        Usage: all <optional <class> >
+        Usage: all <optional <class name> >
         Print all string representation of all instances based or not on class
         """
         all_objects = []
@@ -105,10 +105,71 @@ class HBNBCommand(cmd.Cmd):
                 for key, value in models.storage.all().items():
                     if (line in key):
                         all_objects.append(str(value))
-            print(all_objects)   
-            
+            print(all_objects)
 
-        
+    def do_update(self, line):
+        """
+        Usage: update <class name> <id> <attribute name> <attribute value>
+        Update an instance based on the class name and id by adding or update a attribute
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+            return
+
+        all_objects = models.storage.all()
+
+        rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(rex, line)
+        classname = match.group(1)
+        ins_id = match.group(2)
+        attribute = match.group(3)
+        value = match.group(4)
+
+        if not match:
+            print("** class name missing **")
+        elif classname not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        elif ins_id is None:
+            print("** instance id missing **")
+        else:
+            key = classname + '.' + ins_id
+            if key not in all_objects.keys():
+                print("** no instance found **")
+            else:
+                if not attribute:
+                    print("** attribute name missing **")
+                elif attribute in ['id', 'created_at', 'updated_at']:
+                    print(f"** attribute \"{attribute}\" can't be updated **")
+                else:
+                    if not value:
+                        print("** value missing **")
+                    else:
+                        obj = all_objects[key]
+                        if attribute in obj.__dict__.keys():
+                            if not re.search('^".*"$', value):
+                                try:
+                                    if '.' in value:
+                                        value = float(value)
+                                    else:
+                                        value = int(value)
+                                except ValueError:
+                                    print("** value missing **")
+                                    return
+                            else:
+                                value = value.replace('"','')
+                            if type(value) == type(obj.__dict__[attribute]):
+                                obj.__dict__[attribute] = value
+                                print("update")
+                            else:
+                                print("value error")
+                        else:
+                            if re.search('^".*"$', value):
+                                value = value.replace('"','')
+                                obj.__dict__[attribute] = value
+                                print("update")
+                            else:
+                                print("** value missing **")                       
+    
     def emptyline(self):
         """Doesn't do anything on ENTER."""
         pass
